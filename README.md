1b8ae61af1e17f31c0a13115ca9d98039ee4f5d1
Dongrammer <141012107+Dongrammer@users.noreply.github.com>
2024-02-26 11:18:00 +0900

    초기 커밋 메시지입니다.

---
+++ README.md
@@ -0,0 +1,3 @@
+# UE5_CPP
+
+Developed with Unreal Engine 5
---
+++ Source/UE5_CPP/UE5_CPPGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_CPPGameMode.h"
+#include "UE5_CPPCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AUE5_CPPGameMode::AUE5_CPPGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
